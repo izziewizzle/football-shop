6c4dc63ccd56be548634f45ccbe382b9322c05a9
izziewizzle <izzati.mhr@gmail.com>
2025-09-15 22:50:24 +0700

    character development #proudT-T

--- main/models.py
+++ main/models.py
@@ -4,11 +4,19 @@ from django.db import models
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('clothing', 'Clothing'),
+        ('footwear', 'Footwear'),
+        ('equipment', 'Equipment'),
+        ('accessories', 'Accessories'),
+        ('merchandise', 'Merchandise'),
+    ]
+
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    category = models.CharField(max_length=255)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
     
     def __str__(self):
